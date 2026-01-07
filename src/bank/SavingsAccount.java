f01762ca76aaf5781204b20d36f10c512a1c0a21
Vaishnavi Bandaru <vaishnavibvks9@gmail.com>
2026-01-07 22:50:23 +0530

    Update SavingsAccount.java

--- src/bank/SavingsAccount.java
+++ src/bank/SavingsAccount.java
@@ -1,12 +1,8 @@
 package bank;
-
 public class SavingsAccount extends Account {
-
     public SavingsAccount(int accountNumber, String holderName, double balance) {
         super(accountNumber, holderName, balance);
     }
-
-    @Override
     public void deposit(double amount) {
         if (amount > 0) {
             balance += amount;
@@ -15,8 +11,6 @@ public class SavingsAccount extends Account {
             System.out.println("Invalid deposit amount.");
         }
     }
-
-    @Override
     public void withdraw(double amount) throws InsufficientBalanceException {
         if (amount > balance) {
             throw new InsufficientBalanceException("Insufficient balance!");
